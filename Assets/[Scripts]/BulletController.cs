dece9427caacc5dd3349c34b0cec695e097f62fc
predator1245 <jashshah37@yahoo.com>
2021-10-21 21:59:53 +0200

    Change the bullet fire from vertical to horizontal

--- Assets/[Scripts]/BulletController.cs
+++ Assets/[Scripts]/BulletController.cs
@@ -1,11 +1,21 @@
-﻿using System.Collections;
+﻿/**
+ * Filename:            BulletController.cs
+ * Student Name:        Jash Shah
+ * Student ID:          101274212
+ * Date last modified:  21/10/2021
+ * Program Description: Controls the bullets fired by the player character
+ * Revision History:
+ *  - 21/10/2021 - Change the bullet direction from vertical to horizontal
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletController : MonoBehaviour, IApplyDamage
 {
-    public float verticalSpeed;
-    public float verticalBoundary;
+    public float horizontalSpeed;
+    public float horizontalBoundary;
     public BulletManager bulletManager;
     public int damage;
     
@@ -24,12 +34,12 @@ public class BulletController : MonoBehaviour, IApplyDamage
 
     private void _Move()
     {
-        transform.position += new Vector3(0.0f, verticalSpeed, 0.0f) * Time.deltaTime;
+        transform.position += new Vector3(horizontalSpeed, 0.0f, 0.0f) * Time.deltaTime;
     }
 
     private void _CheckBounds()
     {
-        if (transform.position.y > verticalBoundary)
+        if (transform.position.x > horizontalBoundary)
         {
             bulletManager.ReturnBullet(gameObject);
         }
