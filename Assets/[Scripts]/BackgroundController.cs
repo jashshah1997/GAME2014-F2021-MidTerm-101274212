31c3674154bfb90fb5fb5618346e002b5db64377
predator1245 <jashshah37@yahoo.com>
2021-10-21 21:18:29 +0200

    Make background scroll horizontally

--- Assets/[Scripts]/BackgroundController.cs
+++ Assets/[Scripts]/BackgroundController.cs
@@ -1,11 +1,21 @@
-﻿using System.Collections;
+﻿/**
+ * Filename:            BackgroundController.cs
+ * Student Name:        Jash Shah
+ * Student ID:          101274212
+ * Date last modified:  21/10/2021
+ * Program Description: Controlls the Scrolling background
+ * Revision History:
+ *  - 21/10/2021 - Make the background scroll horizontally
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BackgroundController : MonoBehaviour
 {
-    public float verticalSpeed;
-    public float verticalBoundary;
+    public float horizontalSpeed;
+    public float horizontalBoundary;
 
     // Update is called once per frame
     void Update()
@@ -16,18 +26,18 @@ public class BackgroundController : MonoBehaviour
 
     private void _Reset()
     {
-        transform.position = new Vector3(0.0f, verticalBoundary);
+        transform.position = new Vector3(horizontalBoundary, 0.0f);
     }
 
     private void _Move()
     {
-        transform.position -= new Vector3(0.0f, verticalSpeed) * Time.deltaTime;
+        transform.position -= new Vector3(horizontalSpeed, 0.0f) * Time.deltaTime;
     }
 
     private void _CheckBounds()
     {
         // if the background is lower than the bottom of the screen then reset
-        if (transform.position.y <= -verticalBoundary)
+        if (transform.position.x <= -horizontalBoundary)
         {
             _Reset();
         }
